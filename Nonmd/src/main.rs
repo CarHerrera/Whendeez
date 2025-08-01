f4be84f14d4c6f126d23734dfe1b31e9454a7f1b
Carlos <herrera4345@gmail.com>
2025-08-01 07:54:05 -0400

    vault backup: 2025-08-01 07:54:05

--- Nonmd/src/main.rs
+++ Nonmd/src/main.rs
@@ -7,21 +7,16 @@ struct FileInfo{
     title: String,
     link: String,
     tags: String,
-    map: String,
-    nade_path: String
+    map: String
 }
 fn i_frame(x: String) -> String {
     let first_half = String::from("<iframe allowFullScreen=True class=\"grenLineUp\"");
-    let src = String::from(" src=\"https://www.youtube.com/embed/");
+    let src = String::from(" src=");
     let second_half = String::from("></iframe>");
 
-    return first_half + &src  + &x + "\"" + &second_half;
+    return first_half + &src + "\"" + &x + "\"" + &second_half;
 }
 fn main() {
-    let cwd = std::env::current_dir();
-    let res = cwd.expect("").display().to_string();
-    let l = res.len() - String::from("Nonmd").len();
-    let whendeez_path = &res[..l];
     let path = Path::new("./Quick Links.csv");
     let contents = path.display();
     let mut files:Vec<FileInfo> = Vec::new();
@@ -38,18 +33,16 @@ fn main() {
         let map = &record[1];
         let tags = &record[2];
         let link = &record[3];
-        let nade_path = whendeez_path.to_owned() + map + "/"+ map + " Nades/";
         // Print a debug version of the record.
         let map = FileInfo{title:title.to_string(),
              link: link.to_string(), 
              tags: tags.to_string(), 
-             map: map.to_string(),
-            nade_path: nade_path.to_string()};
+             map: map.to_string()};
         files.push(map);
     }
 
     for f in files{
-        let result = File::create(f.nade_path + &f.title+".md");
+        let result = File::create(f.title+".md");
         let tag_list:Vec<&str> = f.tags.split(',').collect();
         let nadelen = tag_list[1].len()-1;
         let tags = format!(
@@ -63,13 +56,11 @@ Link: {}
 ---
 
 {}
-", tags, f.map, f.link, i_frame(f.link[17..28].to_string()));
+", tags, f.map, f.link, i_frame(f.link[33..].to_string()));
         println!("{}", texrt);
         result.expect("Should be able to write to file").write_all(texrt.as_bytes());
     }
-    let mut t = File::create(&path).expect("Couldn't open file");
-    t.write_all(String::from("Title,Map,Tags,Link").as_bytes()).expect("");
-
+    
 }
 
 // https://youtu.be/-gpc5Raf7zk?si=zLUwWHCffAm_ioYB
